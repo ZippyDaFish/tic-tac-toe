57ee37e769d1aac719bf4d5a8270a3c86ccb1986
ZippyDaFish <andrew.riker21@gmail.com>
2022-09-09 11:36:17 -0400

    Add row and column win checks

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const board = (() => {
         piece.textContent = text;
     };
     console.log(gameBoard);
-    return{createBoard, editPiece};
+    return{gameBoard, createBoard, editPiece};
 })();
 
 const playerFactory = (key) => {
@@ -32,12 +32,59 @@ const boardChecks = (() => {
     const turnCheck = (piece) => {
         if(piece.textContent !== ""){return;} // return if piece is already used
         playerTurn.playTurn(piece); // change piece depending on player turn
+        winCheck();
         // swap player turn
         if(playerTurn == player1){playerTurn = player2;}
         else if(playerTurn == player2){playerTurn = player1;}
         console.log(playerTurn);
+
     };
-    return{playerTurn, turnCheck};
+    const checkRow =() => {
+        console.log("Check row");
+        //check row
+        let consec = 0;
+        for(i = 0; i < 3; i++){
+            for(n = 0; n < 3; n++){
+                if(board.gameBoard[i*3+n].textContent == playerTurn.key){
+                    consec++;
+                }
+            }
+            if(consec >= 3){
+                return true;
+            }
+            consec = 0;
+        }
+        return false;
+    }
+    const checkCol = () => {
+        console.log("Check col");
+        //check col
+        let consec = 0;
+        for(i = 0; i < 3; i++){
+            for(n = 0; n < 3; n++){
+                if(board.gameBoard[n*3+i].textContent == playerTurn.key){
+                    consec++;
+                }
+            }
+            if(consec >= 3){
+                return true;
+            }
+            consec = 0;
+        }
+        return false;
+    }
+    const checkDiag = () => {
+        console.log("MMMM Diagonals");
+        return false;
+    }
+    const winCheck = () => {
+        const checks = [boardChecks.checkRow(), boardChecks.checkCol(), boardChecks.checkDiag()];
+        for(i = 0; i < 3; i++){
+            let win = checks[i];
+            if(win == true){console.log(playerTurn.key + " Wins"); break;}
+        }
+    }
+    return{playerTurn, turnCheck, checkRow, checkCol, checkDiag};
 })();
 
 board.createBoard();
